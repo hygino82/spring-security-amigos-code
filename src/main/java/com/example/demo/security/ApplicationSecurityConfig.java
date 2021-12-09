ca7439e2ce716c40527a432332eea6c2dc77c2f0
Adroaldo <hygino@msn.com>
2021-12-09 20:40:53 -0300

    Role based authentication

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.example.demo.security.ApplicationUserRole.*;
+
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,19 +27,30 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "index", "/css/*", "/js/*").permitAll().anyRequest().authenticated()
+		http.authorizeRequests().antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+		.antMatchers("/api/**").hasRole(STUDENT.name())
+		.anyRequest().authenticated()
 				.and().httpBasic();
 	}
 
 	@Override
 	@Bean
 	protected UserDetailsService userDetailsService() {
-		UserDetails dilmaUser = User.builder()
-				.username("dilma")
-				.password(passwordEncoder.encode("opressora"))
-				.roles("STUDENT")
+		UserDetails annaSmithUser = User.builder()
+				.username("annasmith")
+				.password(passwordEncoder.encode("password"))
+				.roles(STUDENT.name())
 				.build();
-
-		return new InMemoryUserDetailsManager(dilmaUser);
+		
+		UserDetails lindaUser = User.builder()
+				.username("linda")
+				.password(passwordEncoder.encode("password123"))
+				.roles(ADMIN.name())
+				.build();
+		
+		return new InMemoryUserDetailsManager(
+				annaSmithUser,
+				lindaUser
+		);
 	}
 }
