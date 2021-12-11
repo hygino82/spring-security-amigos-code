f5e735b8be686c756b3da296f01a52e78633891d
Adroaldo <hygino@msn.com>
2021-12-11 08:41:41 -0300

    Pre authorize

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -3,7 +3,8 @@ package com.example.demo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+//import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,10 +16,11 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import static com.example.demo.security.ApplicationUserRole.*;
 
-import static com.example.demo.security.ApplicationUserPermission.*;
+//import static com.example.demo.security.ApplicationUserPermission.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private final PasswordEncoder passwordEncoder;
@@ -35,12 +37,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 		.authorizeRequests()
 		.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
 		.antMatchers("/api/**").hasRole(STUDENT.name())
-		.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-		.antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-		.antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-		.antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
-		.anyRequest().authenticated()
-				.and().httpBasic();
+//		.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//		.antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//		.antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//		.antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+		.anyRequest()
+		.authenticated()
+		.and()
+		.httpBasic();
 	}
 
 	@Override
--- src/main/java/com/example/demo/student/StudentManagementController.java
+++ src/main/java/com/example/demo/student/StudentManagementController.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,12 +23,14 @@ public class StudentManagementController {
 	 );
 	 
 	 @GetMapping
+	 @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
 	 public List<Student> getAllStudents(){
 		 System.out.println("Get all students");
 		 return STUDENTS;
 	 }
 	 
 	 @PostMapping
+	 @PreAuthorize("hasAuthority('student:write')")
 	 public void registerNewStudent(@RequestBody Student student) {	 
 		 System.out.println("Register new student");
 		 System.out.println(student);
@@ -35,12 +38,14 @@ public class StudentManagementController {
 	 }
 	 
 	 @DeleteMapping(path = "{studentId}")
+	 @PreAuthorize("hasAuthority('student:write')")
 	 public void deleteStudent(@PathVariable("studentId") Integer studentId) {
 		 System.out.println("Delete student");
 		 System.out.println(studentId);
 	 }
 	 
 	 @PutMapping(path = "{studentId}")
+	 @PreAuthorize("hasAuthority('student:write')")
 	 public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
 		 System.out.println("Update student");
 		 System.out.println(String.format("%s %s", studentId, student));
