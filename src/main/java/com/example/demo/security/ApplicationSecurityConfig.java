872a78d72c24e62589e4210161a2c1133044534a
Adroaldo <hygino@msn.com>
2021-12-11 07:38:30 -0300

    Has authority

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import static com.example.demo.security.ApplicationUserRole.*;
+
+import static com.example.demo.security.ApplicationUserPermission.*;
 
 @Configuration
 @EnableWebSecurity
@@ -27,8 +30,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+		http
+		.csrf().disable()//TODO será visto na próxima aula
+		.authorizeRequests()
+		.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
 		.antMatchers("/api/**").hasRole(STUDENT.name())
+		.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+		.antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+		.antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+		.antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
 		.anyRequest().authenticated()
 				.and().httpBasic();
 	}
--- src/main/java/com/example/demo/student/StudentManagementController.java
+++ src/main/java/com/example/demo/student/StudentManagementController.java
@@ -23,22 +23,26 @@ public class StudentManagementController {
 	 
 	 @GetMapping
 	 public List<Student> getAllStudents(){
+		 System.out.println("Get all students");
 		 return STUDENTS;
 	 }
 	 
 	 @PostMapping
-	 public void registerNewStudent(@RequestBody Student student) {
+	 public void registerNewStudent(@RequestBody Student student) {	 
+		 System.out.println("Register new student");
 		 System.out.println(student);
 		 
 	 }
 	 
 	 @DeleteMapping(path = "{studentId}")
 	 public void deleteStudent(@PathVariable("studentId") Integer studentId) {
+		 System.out.println("Delete student");
 		 System.out.println(studentId);
 	 }
 	 
 	 @PutMapping(path = "{studentId}")
 	 public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
+		 System.out.println("Update student");
 		 System.out.println(String.format("%s %s", studentId, student));
 	 }
 }
