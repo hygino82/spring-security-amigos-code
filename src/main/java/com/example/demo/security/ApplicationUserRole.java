584f0cdf9d5d801acfd2c3137cc1c47a4db7bd46
Adroaldo <hygino@msn.com>
2021-12-11 08:14:19 -0300

    Permission based authentication in action

--- src/main/java/com/example/demo/security/ApplicationUserRole.java
+++ src/main/java/com/example/demo/security/ApplicationUserRole.java
@@ -1,10 +1,16 @@
 package com.example.demo.security;
 
+import static com.example.demo.security.ApplicationUserPermission.COURSE_READ;
+import static com.example.demo.security.ApplicationUserPermission.COURSE_WRITE;
+import static com.example.demo.security.ApplicationUserPermission.STUDENT_READ;
+import static com.example.demo.security.ApplicationUserPermission.STUDENT_WRITE;
+
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import com.google.common.collect.Sets;
-
-import static com.example.demo.security.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
 	STUDENT(Sets.newHashSet()), 
@@ -18,6 +24,15 @@ public enum ApplicationUserRole {
 	}
 
 	public Set<ApplicationUserPermission> getPermissions() {
+		return permissions;
+	}
+	
+	public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+		Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+			.map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+			.collect(Collectors.toSet());
+		permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+		
 		return permissions;
 	}
 }
