82205ca5f541e5a550a5c433afc5994d9ea15946
Adroaldo <hygino@msn.com>
2021-12-13 08:14:10 -0300

    Logout (teste)

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.demo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,10 +12,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static com.example.demo.security.ApplicationUserRole.*;
 
-//import static com.example.demo.security.ApplicationUserPermission.*;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebSecurity
@@ -33,18 +33,32 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-		.csrf().disable()//TODO será visto na próxima aula
-		.authorizeRequests()
-		.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
-		.antMatchers("/api/**").hasRole(STUDENT.name())
-//		.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//		.antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//		.antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//		.antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
-		.anyRequest()
-		.authenticated()
-		.and()
-		.httpBasic();
+			//.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			//.and()
+			.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+			.antMatchers("/api/**").hasRole(STUDENT.name())
+			.anyRequest()
+			.authenticated()
+			.and()
+			.formLogin()
+			.loginPage("/login").permitAll()
+			.defaultSuccessUrl("/courses", true)
+			.and()
+			.rememberMe()
+				.tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+				.key("somethingverysecured")
+			.and()
+			.logout()
+				.logoutUrl("/logout")
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+				.clearAuthentication(true)
+				.invalidateHttpSession(true)
+				.deleteCookies("JSESSIONID", "remember-me")
+				.logoutSuccessUrl("/login");
+				
+			
 	}
 
 	@Override
